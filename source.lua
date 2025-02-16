6e6be5b68eb721c8db4521836a7ae3af6a6ccfdc
frzfrsy <fariezputra506@gmail.com>
2025-02-16 21:10:38 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -7,6 +7,14 @@ if game:GetService("CoreGui"):FindFirstChild("School") then
 	notifSound.PlayOnRemove = true
 	notifSound:Destroy()
 	game.StarterGui:SetCore("SendNotification", {Title = "School", Text = "Executed with no duplication", Icon = "rbxassetid://505845268", Duration = 5})
+else
+	local notifSound2 = Instance.new("Sound", workspace)
+	notifSound2.PlaybackSpeed = 1
+	notifSound2.Volume = 0.15
+	notifSound2.SoundId = "rbxassetid://170765130"
+	notifSound2.PlayOnRemove = true
+	notifSound2:Destroy()
+	game.StarterGui:SetCore("SendNotification", {Title = "School", Text = "Appears in", Icon = "rbxassetid://505845268", Duration = 5})
 end
 
 local Lib = {}
